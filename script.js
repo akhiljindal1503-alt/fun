54557f22785f59cd83b111aacf8ec457f580b70f
akhiljindal1503-alt <akhiljindal1503@gmail.com>
2026-05-29 21:43:43 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function showLove() {
+  const box = document.getElementById("loveBox");
+
+  box.style.display = "block";
+
+  setTimeout(() => {
+    alert("Congratulations 🎉 You have successfully discovered that Dikshit loves Anshi TOO MUCH ❤️😂");
+  }, 500);
+}
